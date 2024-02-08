10f49d4d95f40bac6713a505c8d9ee7f83f68193
Igor Korolevich <ikorolevich4@gmail.com>
2024-02-08 12:30:31 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,114 @@
+﻿namespace bsuirTesting
+{
+    internal class Program
+    {
+        static class myClass
+        {
+            public static double calclulate(double x)
+            {
+                if (x == 1)
+                {
+                    return 4;
+                }
+                return (Math.Pow(x + 1, 2) / x) + myClass.calclulate(x - 1);
+            }
+            public static int FindMax(int i, int[] arr)
+            {
+                if (i == 9)
+                {
+                    return arr[i];
+                }
+
+                if (arr[i] > FindMax(i + 1, arr))
+                {
+                    return arr[i];
+
+                }
+                else
+                {
+                    return FindMax(i + 1, arr);
+                }
+
+            }
+            public static double CalcSummAll(int i, int[] arr)
+            {
+
+                if (i == 9)
+                {
+                    return arr[i];
+                }
+
+                return arr[i] + CalcSummAll(i + 1, arr);
+            }
+            public static void SortArray(int[] arr)
+            {
+                int tmpVal;
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    for ( int j = i + 1; j < arr.Length; j++)
+                    {
+                        if (arr[j] < arr[i])
+                        {
+                            tmpVal = arr[i];
+                            arr[i] = arr[j];
+                            arr[j] = tmpVal;
+                        }
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+
+
+            int n = 50;
+            int randomRange = 100;
+            int[] myArray = new int[n];
+            double sum = 0;
+
+
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                myArray[i] = rnd.Next(1, randomRange);
+            }
+
+            myClass.SortArray(myArray);
+
+
+
+            for (int i = 0; i < n; i++)
+            {
+                sum += myArray[i];
+            }
+            sum = 0;
+            sum = myClass.CalcSummAll(0, myArray);
+
+            int maxVal = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (maxVal < myArray[i])
+                {
+                    maxVal = myArray[i];
+                }
+            }
+            Console.WriteLine(maxVal);
+            int r = myClass.FindMax(0, myArray);
+
+
+            double S = 0;
+            double R = 0;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                S += Math.Pow(i + 1, 2) / i;
+            }
+
+            Console.WriteLine(S);
+
+            R = myClass.calclulate(n);
+
+            Console.WriteLine(R);
+        }
+    }
+}
